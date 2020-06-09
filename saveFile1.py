e634670329d4772c51b410485464dca1a5eec4e7
froemosen <kristianantonhedegaard@gmail.com>
2020-06-09 21:41:01 +0200

    Savefile ready

--- saveFile1.py
+++ saveFile1.py
@@ -1,7 +1,7 @@
 import Classes
 import Variabler
-x = 1125
-y = 179
+x = 700
+y = 7
 smark = Classes.smark(x, y)
 smark.walkDown = False
 smark.walkUp = False
@@ -9,9 +9,9 @@ smark.walkRigth = False
 smark.walkLeft = True
 smark.stand = True
 walking = False
-scene = 2
-Variabler.health = 928
+scene = 0
+Variabler.health = 1000
 Variabler.pizza = 0
 Variabler.burger = 0
 Variabler.kaffe = 0
-Variabler.energidrik = 11
+Variabler.energidrik = 0
